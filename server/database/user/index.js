813745c4a6c76deae6f0494ee0b99d9dca95a293
ISHA <johndoe@example.com>
2021-09-05 13:27:15 +0530

    add statics and methods

--- server/database/user/index.js
+++ server/database/user/index.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 const UserSchema = new mongoose.Schema({
     fullname: {type: String, required:true},
@@ -7,6 +9,47 @@ const UserSchema = new mongoose.Schema({
     address: [{detail: {type: String }, for: {type: String }}],
     phoneNumber: [{type: Number }],
 
+},
+{
+    timestamps: true,
+}
+);
+
+UserSchema.methods.generateJwtToken = function () {
+    return jwt.sign({user: this._id.toString() }, "ZomatoAPP");
+}
+
+UserSchema.statics.findByEmailAndPhone = async ({email, phoneNumber}) => {
+    //check whether email exist
+    const checkUserByEmail = await UserModel.findOne({email});
+    const checkUserByPhone = await UserModel.findOne({phoneNumber});
+
+    if (checkUserByEmail || checkUserByPhone) {
+        throw new Error("User already exist!");
+    }
+
+    return false;
+};
+
+UserSchema.pre("save", function( next )  {
+    const user = this;
+
+    //password is modified
+    if(!user.isModified("password")) return next();
+
+    //generate bcrypt salt
+    bcrypt.genSalt(8, (error, salt) => {
+        if(error) return next(error);
+
+        //hash the password
+        bcrypt.hash(user.password, salt, (error, hash) => {
+            if(error) return next(error);
+
+            //assigning the hashed password
+            user.password = hash;
+            return next();
+        });
+    });
 });
 
 export const UserModel = mongoose.model("Users", UserSchema);
--- server/index.js
+++ server/index.js
@@ -1,6 +1,16 @@
+// import Env variables
+require("dotenv").config();
+
+//Libraries
 import express  from "express";
 import cors from "cors";
 import helmet from "helmet";
+
+//microservice routes
+import Auth from "./API/Auth";
+
+// Database connection
+import ConnectDB from "./database/connection";
 
 const zomato = express();
 
@@ -10,6 +20,13 @@ zomato.use(express.urlencoded({extended: false}));
 zomato.use(helmet());
 zomato.use(cors());
 
+//Application routes
+zomato.use("/auth", Auth);
+
 zomato.get("/", (req,res) => res.json({message: "setup success"}));
 
-zomato.listen(4000, () => console.log("server is running!"));
+zomato.listen(4000, () => 
+    ConnectDB()
+        .then(() => console.log("server is running!"))
+        .catch(() => console.log("server is running but database connection failed!"))
+);
