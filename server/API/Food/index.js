818e1d93c9a6573c228dbe94f0587eb3caefcbc6
ISHA <johndoe@example.com>
2021-10-05 16:07:07 +0530

    add order online food list reducer

--- server/API/Food/index.js
+++ server/API/Food/index.js
@@ -34,14 +34,32 @@ Router.get("/r/:_id" , async (req, res) => {
 })
 
 /*
+Route     /:_id
+Des       Get food based on id
+Params    _id
+Access    Public
+Method    GET  
+*/
+Router.get("/:_id", async (req, res) => {
+    try {
+      const { _id } = req.params;
+      const foods = await FoodModel.findById(_id);
+  
+      return res.json({ foods });
+    } catch (error) {
+      return res.status(500).json({ error: error.message });
+    }
+  });
+
+/*
 Route   /c
-Des     Get all food based on particular restaurant
-Params  id
+Des     Get all food based on particular category
+Params  category
 Access  Public
 Method  GET
 */
 
-Router.get("/r/:_category" , async (req, res) => {
+Router.get("/r/:category" , async (req, res) => {
     try {
 
         await Validatecategory(req.params);
