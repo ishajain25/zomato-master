de24a5bf0b2059a7ca057609bf21be0a22aac15b
ISHA <johndoe@example.com>
2021-10-14 01:53:04 +0530

    modify navbar and add auth

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,7 @@
 import { Route, Redirect, Switch  } from "react-router-dom";
 import axios from "axios";
+import { useDispatch } from "react-redux";
+import React, { useEffect } from "react";
 
 //import HOC
 import HomeLayoutHOC from "./HOC/Home.Hoc";
@@ -19,6 +21,9 @@ import Checkout from "./Page/Checkout";
 import RedirectRestaurant from "./Page/Restaurant/Redirect";
 import GoogleAuth from "./Page/GoogleAuth";
 
+// redux action
+import { getMyself } from "./Redux/Reducer/User/user.action";
+
 // axios global settings
 if (localStorage.zomatoUser) {
   const { token } = JSON.parse(localStorage.zomatoUser);
@@ -26,6 +31,12 @@ if (localStorage.zomatoUser) {
 }
 
 function App() {
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (localStorage.zomatoUser) dispatch(getMyself());
+  }, []);
+  
   return (
     <> 
         <Route path="/" exact>
@@ -46,7 +57,7 @@ function App() {
         exact 
         component={GoogleAuth} 
         />
-        
+
         <RestaurantLayoutHOC
         path="/restaurant/:id/overview"
         exact
